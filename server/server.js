c5122d0d9631100a39f4c1c45d3054a157ac2d28
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-07-14 20:22:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://portfolio-nxkm.vercel.app' }));
+app.use(cors({ origin: 'https://portfolio-f78y.onrender.com' }));
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
