fd1499728415af6b5e88861b108a5643cba79acb
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-07-14 20:06:05 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://portfolio-f78y.onrender.com/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
