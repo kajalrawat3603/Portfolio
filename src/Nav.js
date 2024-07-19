80e7316e7300a0b9408e9d969fc41b3021fbb882
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-07-20 02:06:42 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -27,7 +27,7 @@ const Navbar = () => {
         </li>
         <li>
           <a
-            href="https://drive.google.com/file/d/1ByE_xANd6arMPa2PUjwUdbOvHWZlCIrO/view?usp=sharing"
+            href="https://drive.google.com/file/d/1a_nNL-sBodhzx7V_wPWBq4F8QI2AupUL/view?usp=sharing"
             className="link"
             target="_blank"
             rel="noopener noreferrer"
